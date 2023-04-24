799af2c8198d7f0573c3078eab1489720364cf1c
Fatema7843 <fatematuz784@gmail.com>
2023-04-24 17:52:54 +0600

    commit

---
+++ Zero Hunger/Zero Hunger/Controllers/PersonController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zero_Hunger.Models;
+
+namespace Zero_Hunger.Controllers
+{
+    public class PersonController : Controller
+    {
+        public ActionResult List()
+        {
+            Student s1 = new Student()
+            {
+                Name = "fatema",
+                Id = "123",
+                DoB = "1.1.2000",
+
+            };
+
+           /* string[] names=new string[3];
+            names[0] = "names1";
+            names[1] = "id";
+            names[2] = "DoB";
+            ViewBag.Names = names;*/
+
+            return View(s1);
+
+        }
+    }
+}
---
+++ Zero Hunger/Zero Hunger/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zero_Hunger.Models
+{
+    public class Student
+    {
+       public string Name { get; set; }
+       public string Id { get; set; }
+       public string DoB { get; set; }
+
+    }
+}
